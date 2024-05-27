b2483a8d8c5fd26eee1d05fa85a3213e1f8f1864
남상엽 <spearoad15@gmail.com>
2024-05-27 10:15:55 +0900

    swagger server url 삭제

--- backend/src/main/java/com/ssafy/mugit/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/mugit/global/config/SwaggerConfig.java
@@ -4,9 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(servers = {
-        @Server(url = "https://mugit.site", description = "Default Server URL"),
-        @Server(url = "http://localhost:8080", description = "Default Local URL")})
+@OpenAPIDefinition(servers = {@Server(url = "http://localhost:8080", description = "Default Local URL")})
 @Configuration
 public class SwaggerConfig {
 }
